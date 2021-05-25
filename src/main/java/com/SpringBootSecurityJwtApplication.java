86411384ae631a31b701cc1c9e9eb2891ef03df2
SERA Trust <84803361+infoseratrust@users.noreply.github.com>
2021-05-26 00:13:59 +0530

    Update SpringBootSecurityJwtApplication.java

--- src/main/java/com/SpringBootSecurityJwtApplication.java
+++ src/main/java/com/SpringBootSecurityJwtApplication.java
@@ -11,6 +11,7 @@ public class SpringBootSecurityJwtApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
+        
     }
 
 
